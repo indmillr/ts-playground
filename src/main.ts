6c041e51d28d83dc52fafc37bd027311174543d9
Ian Miller <theiandmiller@gmail.com>
2022-08-09 06:07:07 -0500

    never

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,3 @@
-// ----- VOID Type ----- //
-
-const doSomething = (): void => {
-  console.log("doSomething");
+const doSomething = (): never => {
+  throw "never";
 };
-
-let foo: any = "foo"; // 'any' turns off TS check -- try to avoid ANY
-// ANY is not a solution, but a cause to bigger future problems
-console.log(foo.bar());
