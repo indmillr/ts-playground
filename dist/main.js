c659beb27ee4dd27c7054734933110916dc4296c
Ian Miller <theiandmiller@gmail.com>
2022-08-09 05:37:02 -0500

    interfaces

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,13 @@
+// Function syntax
 var getFullName = function (name, surname) {
     return (name = " " + surname);
 };
 console.log(getFullName("Ian", "Miller"));
+var user = {
+    name: "Ian",
+    age: 37
+};
+var user2 = {
+    name: "Jack"
+};
+console.log(user.name);
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,27 @@
+// Function syntax
+
 const getFullName = (name: string, surname: string): string => {
   return (name = " " + surname);
 };
 
 console.log(getFullName("Ian", "Miller"));
+
+// ----- Interfaces ----- //
+
+// this is a reusable interface for all 'users'
+// by default, all props are mandatory
+interface User {
+  name: string;
+  age?: number; // the '?' makes the 'age' prop optional
+}
+
+const user: User = {
+  name: "Ian",
+  age: 37,
+};
+
+const user2: User = {
+  name: "Jack",
+};
+
+console.log(user.name);
